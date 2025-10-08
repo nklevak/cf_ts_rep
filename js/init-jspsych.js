e8ff1f5e472632037cc8cf7bdcad2102d4453463
nklevak <nastasia.k1214@gmail.com>
2025-10-08 12:01:59 -0700

    updated so that it works on proliferate

--- js/init-jspsych.js
+++ js/init-jspsych.js
@@ -2,19 +2,21 @@
 var jsPsych = initJsPsych({
   show_progress_bar: true,
   auto_update_progress_bar: false,
-  on_finish: function() {
+  // on_finish: function() {
     
-    // Add interactions to the data variable
-    //var interaction_data = jsPsych.data.getInteractionData();
-    //jsPsych.data.get().addToLast({interactions: interaction_data.json()});
+  //   // Add interactions to the data variable
+  //   //var interaction_data = jsPsych.data.getInteractionData();
+  //   //jsPsych.data.get().addToLast({interactions: interaction_data.json()});
 
-    // Display jsPsych data in viewport.
-    //jsPsych.data.displayData();
-  },
+  //   // Display jsPsych data in viewport.
+  //   //jsPsych.data.displayData();
+  // },
   on_start: function(){
     //document.getElementById("jspsych-progressbar-container").style.visibility = "hidden";
   },
   on_finish: function(){
+    var experimentData = jsPsych.data.get().json();
+    proliferate.submit(experimentData)
     window.location = "https://app.prolific.com/submissions/complete?cc=CEAKMHC3"
   }
 });
